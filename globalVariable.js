e7454ab83d09d06333ddedfe159ca765d301b32f
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2024-09-26 12:18:36 +0300

    global object property

--- globalVariable.js
+++ globalVariable.js
@@ -1,4 +1,10 @@
-console.log(process)
-console.log(process.argv)
-console.log(process.env)
-
+// console.log(process)
+// console.log(process.argv)
+// console.log(process.env)
+console.log(__dirname)
+console.log(__filename)
+const startTime = process.hrtime();
+//console.log(startTime)
+for(let i =0; i < 10; i++){}
+const endTime = process.hrtime(startTime);
+console.log("Time it took ",endTime[1]/1000000)
